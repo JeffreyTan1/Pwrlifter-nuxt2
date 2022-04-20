73dd99a8383d5de3c4a2c9a2d9f92fae2f733461
Jeffrey Tan <S3851781@student.rmit.edu.au>
2022-04-20 23:02:46 +1000

    dashboard embed

--- pages/analytics/dashboard.vue
+++ pages/analytics/dashboard.vue
@@ -1,8 +1,10 @@
 <template>
-  <div class="min-h-screen min-w-screen flex justify-center align-middle">
-    <div class="flex flex-col w-1/3 mx-auto my-auto text-center">
-      
-    </div>
+  <div class="min-h-screen min-w-screen flex justify-center items-center">
+    <iframe
+        width="960"
+        height="720"
+        src="https://ap-southeast-2.quicksight.aws.amazon.com/sn/embed/share/accounts/672162749407/dashboards/fcb286da-2a59-437b-855b-5a770ed79554?directory_alias=pwrlifter-admin">
+    </iframe>
   </div>
 </template>
 
